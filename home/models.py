3cf5ac6dad1af3cea1cd2655626fd200131a7cd7
LomtikKomsa <lomtik.01@mail.ru>
2022-06-28 23:26:30 +1000

    Some changes

--- home/models.py
+++ home/models.py
@@ -5,9 +5,10 @@ from modelcluster.fields import ParentalKey
 from wagtail.models import Page, Orderable
 from wagtail.admin.panels import FieldPanel, InlinePanel
 
+
 class HomePage(Page):
 
-    def get_context(self, request):
+    def get_context(self, request, **kwargs):
         context = super().get_context(request)
         newsindexpage = self.get_first_child()
         newspages = newsindexpage.get_children().live().order_by('-first_published_at')
@@ -20,6 +21,7 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         InlinePanel('carousel_images', label="Gallery images"),
     ]
+
 
 class NewsPageGalleryImage(Orderable):
     page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='carousel_images')
